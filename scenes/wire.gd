334a980d796da269f233e1975ad0e83cfa71e750
pattokseas <mpattok@seas.upenn.edu>
2025-01-07 21:40:52 -0500

    update hooks

--- scenes/wire.gd
+++ scenes/wire.gd
@@ -1,46 +1,59 @@
 extends Node3D
 
-const pts := []
+@onready var wire: MeshInstance3D = $"."
+
+var player = null
+func set_player(p):
+	player = p
+	
+var drawn := false
+
+var local_from := Vector3.ZERO
+var global_to := Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	wire.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	wire.mesh.material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	wire.mesh.material.albedo_color = Color.BLACK
+	wire.mesh.top_radius = 0.025
+	wire.mesh.bottom_radius = 0.025
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if player != null:
+		var from = player.to_global(local_from)
+		update(from, global_to)
 
 func draw_line(from, to):
-	var mesh_instance := MeshInstance3D.new()
-	var cylinder_mesh := CylinderMesh.new()
-	var material := ORMMaterial3D.new()
+	if player == null: return
+	local_from = from
+	global_to = to
+	update(player.to_global(from), global_to)
+	player.get_tree().get_root().add_child(wire)
+	drawn = true
 
-	mesh_instance.mesh = cylinder_mesh
-	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
-	mesh_instance.position = (from + to) * 0.5
+func remove_line():
+	player.get_tree().get_root().remove_child(wire)
+	drawn = false
+
+func update(from, to):
+	wire.rotation = get_rotation_between(from, to)
+	wire.mesh.height = get_distance_between(from, to)
+	wire.global_position = (from + to) * 0.5
+
+func get_rotation_between(from, to):
 	var dir = (from - to).normalized()
 	const up = Vector3(0, 1, 0)
-	if dir == -up:
-		mesh_instance.rotation = Vector3(PI, 0, 0)
+	if dir == up:
+		return Vector3(0, 0, 0)
+	elif dir == -up:
+		return Vector3(PI, 0, 0)
 	else:
-		mesh_instance.rotation = Basis(up, dir).get_euler()
+		return Basis(Quaternion(up, dir)).get_euler()
 
-	#immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
-	#immediate_mesh.surface_add_vertex(from)
-	#immediate_mesh.surface_add_vertex(to)
-	#immediate_mesh.surface_end()
-	
+func get_distance_between(from, to):
 	var dx = from.x - to.x
 	var dy = from.y - to.y
 	var dz = from.z - to.z
-	var d = sqrt(dx * dx + dy * dy + dz * dz)
-	cylinder_mesh.height = d
-
-	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	material.albedo_color = Color.BLACK
-
-	get_tree().get_root().add_child(mesh_instance)
-	return mesh_instance
-
-func draw():
-	pass
+	return sqrt(dx * dx + dy * dy + dz * dz)
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,63 +4,55 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 const GRAPPLE_DISTANCE = 20.0
-const LEFT_HOOK = Vector3(-0.55, 0, 0)
-const RIGHT_HOOK = Vector3(0.55, 0, 0)
+const LEFT_HOOK = Vector3(-0.5, 0, 0)
+const RIGHT_HOOK = Vector3(0.5, 0, 0)
+const REEL_SPEED = 4.0
 
 const SENSITIVITY = 0.01
-const CAM_RADIUS = 0
-const CAM_OFFSET = Vector3(0, 0.555, 0)
+const CAM_RADIUS = 0.0
+const CAM_OFFSET = Vector3(0, CAM_RADIUS + 0.555, CAM_RADIUS)
 
 @onready var cam: Camera3D = $Camera3D
 @onready var look_raycast: RayCast3D = $Camera3D/LookRaycast
+@onready var left_wire: MeshInstance3D = $LeftWire
+@onready var right_wire: MeshInstance3D = $RightWire
 
 var left_grappled := false
-var left_wire = null
-var left_hooked_to = Vector3(0, 0, 0)
 var right_grappled := false
-var right_wire = null
-var right_hooked_to = Vector3(0, 0, 0)
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_PAUSABLE
 	look_raycast.add_exception(self)
 	look_raycast.target_position = Vector3(0, 0, -1) * GRAPPLE_DISTANCE
-	cam.position.z = CAM_RADIUS
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		var rot_x = event.relative.x * SENSITIVITY
-		var rot_y = event.relative.y * SENSITIVITY
-		rotate_object_local(Vector3(0, -1, 0), rot_x)
-		cam.rotate_object_local(Vector3(-1, 0, 0), rot_y)
-		cam.rotation.x = clamp(cam.rotation.x, -PI/2, PI/2)
-		cam.position.y = CAM_RADIUS * sin(-cam.rotation.x) + CAM_OFFSET.y
-		cam.position.z = CAM_RADIUS * cos(-cam.rotation.x) + CAM_OFFSET.z
+	cam.position.x = CAM_OFFSET.x
+	cam.position.y = CAM_RADIUS * sin(-cam.rotation.x) + CAM_OFFSET.y
+	cam.position.z = CAM_RADIUS * cos(-cam.rotation.x) + CAM_OFFSET.z
+	left_wire.set_player(self)
+	right_wire.set_player(self)
+	remove_child(left_wire)
+	remove_child(right_wire)
 	
 func _process(delta):
-	if not left_grappled and left_wire != null:
-		get_tree().get_root().remove_child(left_wire)
-		left_wire = null
-	if not right_grappled and right_wire != null:
-		get_tree().get_root().remove_child(right_wire)
-		right_wire = null
-	if left_wire != null:
-		update_mesh(left_wire, to_global(LEFT_HOOK), left_hooked_to)
-		if get_distance_between(to_global(LEFT_HOOK), left_hooked_to) > GRAPPLE_DISTANCE + 1.0:
+	if left_wire.drawn:
+		if left_wire.get_distance_between(to_global(LEFT_HOOK), 
+			left_wire.global_to) > GRAPPLE_DISTANCE + 1.0:
 			left_grappled = false
-	if right_wire != null:
-		update_mesh(right_wire, to_global(RIGHT_HOOK), right_hooked_to)
-		if get_distance_between(to_global(RIGHT_HOOK), right_hooked_to) > GRAPPLE_DISTANCE + 1.0:
+		if not left_grappled: left_wire.remove_line()
+	if right_wire.drawn:
+		if right_wire.get_distance_between(to_global(RIGHT_HOOK), 
+			right_wire.global_to) > GRAPPLE_DISTANCE + 1.0:
 			right_grappled = false
+		if not right_grappled: right_wire.remove_line()
+	
 	
 func _physics_process(delta: float) -> void:
 	
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and not is_reeling():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_select") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -77,60 +69,41 @@ func _physics_process(delta: float) -> void:
 	if look_raycast.is_colliding():
 		if Input.is_action_just_pressed("left_mb"):
 			left_grappled = true
-			left_hooked_to = look_raycast.get_collision_point()
-			left_wire = grapple(LEFT_HOOK, look_raycast.get_collision_point())
+			left_wire.draw_line(LEFT_HOOK, look_raycast.get_collision_point())
 		if Input.is_action_just_pressed("right_mb"):
 			right_grappled = true
-			right_hooked_to = look_raycast.get_collision_point()
-			right_wire = grapple(RIGHT_HOOK, look_raycast.get_collision_point())
+			right_wire.draw_line(RIGHT_HOOK, look_raycast.get_collision_point())
 	if Input.is_action_just_released("left_mb"):
 		left_grappled = false
 	if Input.is_action_just_released("right_mb"):
 		right_grappled = false
+		
+	if is_reeling(): velocity.y = 0
+		
+	if left_grappled and Input.is_action_pressed("reel"):
+		var left_vec = (left_wire.global_to - to_global(position)).normalized()
+		velocity.x += left_vec.x * REEL_SPEED
+		velocity.y += left_vec.y * REEL_SPEED
+		velocity.z += left_vec.z * REEL_SPEED
+		
+	if right_grappled and Input.is_action_pressed("reel"):
+		var right_vec = (right_wire.global_to - to_global(position)).normalized()
+		velocity.x += right_vec.x * REEL_SPEED
+		velocity.y += right_vec.y * REEL_SPEED
+		velocity.z += right_vec.z * REEL_SPEED
+		
 	
 	move_and_slide()
 
-func grapple(from, to):
-	return draw_line(to_global(from), to)
+func is_reeling():
+	return Input.is_action_pressed("reel") and (left_grappled or right_grappled)
 
-func update_mesh(mesh, from, to):
-	mesh.rotation = get_rotation_between(from, to)
-	mesh.mesh.height = get_distance_between(from, to)
-	mesh.position = (from + to) * 0.5
-
-func get_rotation_between(from, to):
-	var dir = (from - to).normalized()
-	const up = Vector3(0, 1, 0)
-	if dir == up:
-		return Vector3(0, 0, 0)
-	elif dir == -up:
-		return Vector3(PI, 0, 0)
-	else:
-		return Basis(Quaternion(up, dir)).get_euler()
-
-func get_distance_between(from, to):
-	var dx = from.x - to.x
-	var dy = from.y - to.y
-	var dz = from.z - to.z
-	return sqrt(dx * dx + dy * dy + dz * dz)
-
-func draw_line(from, to):
-	var mesh_instance := MeshInstance3D.new()
-	var cylinder_mesh := CylinderMesh.new()
-	var material := ORMMaterial3D.new()
-
-	mesh_instance.mesh = cylinder_mesh
-	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
-	mesh_instance.position = (from + to) * 0.5
-	mesh_instance.rotation = get_rotation_between(from, to)
-	
-	cylinder_mesh.height = get_distance_between(from, to)
-	cylinder_mesh.top_radius = 0.025
-	cylinder_mesh.bottom_radius = 0.025
-
-	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
-	material.albedo_color = Color.BLACK
-	cylinder_mesh.material = material
-
-	get_tree().get_root().add_child(mesh_instance)
-	return mesh_instance
+func _input(event):
+	if event is InputEventMouseMotion:
+		var rot_x = event.relative.x * SENSITIVITY
+		var rot_y = event.relative.y * SENSITIVITY
+		rotate_object_local(Vector3(0, -1, 0), rot_x)
+		cam.rotate_object_local(Vector3(-1, 0, 0), rot_y)
+		cam.rotation.x = clamp(cam.rotation.x, -PI/2, PI/2)
+		cam.position.y = CAM_RADIUS * sin(-cam.rotation.x) + CAM_OFFSET.y
+		cam.position.z = CAM_RADIUS * cos(-cam.rotation.x) + CAM_OFFSET.z
